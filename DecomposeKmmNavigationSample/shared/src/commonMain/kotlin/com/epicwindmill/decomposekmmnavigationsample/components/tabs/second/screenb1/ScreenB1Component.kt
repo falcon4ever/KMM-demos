195666804896583e5b306b943cdcbd5155681396
Laurence Muller <laurence.muller@gmail.com>
2021-08-04 15:33:24 -0400

    Saving the state of the 'magic' value in case we rotate the device.

--- DecomposeKmmNavigationSample/shared/src/commonMain/kotlin/com/epicwindmill/decomposekmmnavigationsample/components/tabs/second/screenb1/ScreenB1Component.kt
+++ DecomposeKmmNavigationSample/shared/src/commonMain/kotlin/com/epicwindmill/decomposekmmnavigationsample/components/tabs/second/screenb1/ScreenB1Component.kt
@@ -4,6 +4,9 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.decompose.value.reduce
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
+import com.arkivanov.essenty.statekeeper.consume
 import com.epicwindmill.decomposekmmnavigationsample.components.tabs.second.screenb1.IScreenB1.Model
 
 class ScreenB1Component(
@@ -11,15 +14,32 @@ class ScreenB1Component(
     private val navigateToB2: () -> Unit
 ) : IScreenB1, ComponentContext by componentContext {
 
+    private var state: State = stateKeeper.consume(key = "SAVED_STATE") ?: State()
+
     private val _model = MutableValue(Model(magicNumber = 0))
 
     override val model: Value<Model> = _model
+
+    init {
+        stateKeeper.register("SAVED_STATE") { state }
+        _model.reduce {
+            it.copy(magicNumber = state.magicNumber)
+        }
+    }
 
     override fun navigateToB2Clicked() {
         navigateToB2()
     }
 
     override fun onResult(value: Int) {
-        _model.reduce { it.copy(magicNumber = value) }
+        state = State(magicNumber = value)
+        _model.reduce {
+            it.copy(magicNumber = state.magicNumber)
+        }
     }
+
+    @Parcelize
+    private class State(
+        val magicNumber: Int = 0
+    ) : Parcelable
 }
--- DecomposeKmmNavigationSample/shared/src/commonMain/kotlin/com/epicwindmill/decomposekmmnavigationsample/components/tabs/third/screenc1/ScreenC1Component.kt
+++ DecomposeKmmNavigationSample/shared/src/commonMain/kotlin/com/epicwindmill/decomposekmmnavigationsample/components/tabs/third/screenc1/ScreenC1Component.kt
@@ -4,6 +4,9 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.decompose.value.reduce
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
+import com.arkivanov.essenty.statekeeper.consume
 import com.epicwindmill.decomposekmmnavigationsample.components.tabs.third.screenc1.IScreenC1.Model
 
 class ScreenC1Component(
@@ -11,15 +14,32 @@ class ScreenC1Component(
     private val navigateToC2: () -> Unit
 ) : IScreenC1, ComponentContext by componentContext {
 
+    private var state: State = stateKeeper.consume(key = "SAVED_STATE") ?: State()
+
     private val _model = MutableValue(Model(magicNumber = 0))
 
     override val model: Value<Model> = _model
+
+    init {
+        stateKeeper.register("SAVED_STATE") { state }
+        _model.reduce {
+            it.copy(magicNumber = state.magicNumber)
+        }
+    }
 
     override fun navigateToC2Clicked() {
         navigateToC2()
     }
 
     override fun onResult(value: Int) {
-        _model.reduce { it.copy(magicNumber = value) }
+        state = State(magicNumber = value)
+        _model.reduce {
+            it.copy(magicNumber = state.magicNumber)
+        }
     }
+
+    @Parcelize
+    private class State(
+        val magicNumber: Int = 0
+    ) : Parcelable
 }
